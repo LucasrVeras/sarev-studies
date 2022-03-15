370453d0e8527e53e5f1d47514c1d0408ad31f2e
LucasrVeras <lucas.veras01@gmail.com>
2022-03-14 22:00:54 -0300

    Create list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import List from './components/List';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       
-        <Form />
+      <Form />
+      <List />
      
     </div>
   );
