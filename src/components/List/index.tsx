f0288cf697b046a44bc37b5f992461fd453e1390
LucasrVeras <lucas.veras01@gmail.com>
2022-03-14 23:47:57 -0300

    List map

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
+
 function List(){
+	
+	const tasks = [{
+		task: 'react',
+		time: '02:00:00'
+	},{
+		task: 'JavaScript',
+		time: '01:00:00'
+	},{
+		task: 'SpringBoot',
+		time: '01:30:00'
+	}];
+
 	return (
 		<aside>
 			<h2>Studies to day</h2>
 			<ul>
-				<li>
-					<h3>React</h3>
-					<span>
-						02:00:00
-					</span>
-				</li>
-				<li>
-					<h3>JavaScript</h3>
-					<span>
-						01:00:00
-					</span>
-				</li>
+				{tasks.map((item, index) => (
+					<li key={index}>
+						<h3>{item.task}</h3>
+						<span>{item.time}</span>
+					</li>
+				))}
 			</ul>
 		</aside>
 	);
