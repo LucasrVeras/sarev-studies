03882dcaba9401e725bfaa832b4c38118a9ab836
LucasrVeras <lucas.veras01@gmail.com>
2022-03-23 23:19:33 -0300

    Props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Button extends React.Component {
 	render(){
 		return(	
-			<button className="btn btn-secondary btn-lg">Save</button>
+			<button className="btn btn-secondary btn-lg">
+				{this.props.children}
+			</button>
 		);
 	}
 };
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -30,7 +30,9 @@ class Form extends React.Component {
 						max="01:30:00"
 						required/>
 				</div>
-				<Button />
+				<Button>
+					Add
+				</Button>
 			</form>
 		);
 	}
