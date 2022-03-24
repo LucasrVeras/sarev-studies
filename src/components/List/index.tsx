7b7d012bccc7bceebe353d4e4d01cbf357c70d37
LucasrVeras <lucas.veras01@gmail.com>
2022-03-24 17:25:08 -0300

    Introducing states

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item/Index';
 import style from './List.module.scss';
 
 
-
 function List(){
 	
-	const tasks = [{
+	const [tasks, setTasks] = useState([
+	{
 		task: 'react',
 		time: '02:00:00'
 	},{
@@ -15,11 +15,13 @@ function List(){
 	},{
 		task: 'SpringBoot',
 		time: '01:30:00'
-	}];
+	}]);
 
 	return (
 		<aside className={style.listaTarefas}>
-			<h2>Studies to day</h2>
+			<h2 onClick={() => {
+					setTasks([...tasks, { task: "TypeScript", time: "01:30:00" }]);
+				}}>Studies to day</h2>
 			<ul>
 				{tasks.map((item, index) => (
 					<Item
